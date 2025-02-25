8f616d6af677853c1cb6f423e030bf4ac7a7a969
SEB-SBB <info@steigerbouwbrabant.nl>
2025-02-25 22:45:22 +0100

    Add files via upload

--- script.js
+++ script.js
@@ -1,26 +1,3 @@
-function filterProducts() {
-    const aantalPersonen = document.getElementById('aantal-personen').value;
-    const aantalJets = document.getElementById('aantal-jets').value;
-    const ledVerlichting = document.getElementById('led-verlichting').value;
-    const prijs = document.getElementById('prijs').value;
-    const afmetingen = document.getElementById('afmetingen').value;
-
-    let filteredData = jacuzziData.filter(jacuzzi => {
-        // ... (filterlogica voor personen, jets, led) ...
-
-        if (prijs !== 'all') {
-            // Voeg filterlogica voor prijs toe
-        }
-
-        if (afmetingen !== 'all') {
-            // Voeg filterlogica voor afmetingen toe
-        }
-
-        return true; // Indien alle filters passeren
-    });
-
-    displayProducts(filteredData);
-}
 document.addEventListener('DOMContentLoaded', function() {
     const images = [
         'images/jw-2226-1.png',
